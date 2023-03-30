05473db60a6220116a0d183b9e551082c8f7665f
base348 <wanghj@shanghaitech.edu.cn>
2023-03-30 22:23:03 +0800

    para_coll

--- code/utils/generate_firedata.py
+++ code/utils/generate_firedata.py
@@ -1,15 +1,18 @@
 # !! xlrd version=1.2.0 needed
 import numpy as np
 import pandas as pd
+import math
+import json
 
-file_population_density = np.load("data\\population_density.npy")
-# file_regional_industry = np.load("data\\regional_industry.npy")
-# file_weather = np.load("data\\weather.npy")
-file_fire = pd.read_excel("data\\fire.xlsx")
+file_population_density = np.load("data/population_density.npy")
+# file_regional_industry = np.load("data/regional_industry.npy")
+# file_weather = np.load("data/weather.npy")
+file_fire = pd.read_excel("data/fire.xlsx")
 grids = {}
+fire_types = []
 
 def time_to_month(time):
-    return (time.year - 2007)*12 + time.month
+    return (time.year - 2007)*12 + time.month - 1
 
 # UGLY
 def get_grid_id(lat,lng,grids):
@@ -33,29 +36,29 @@ def get_grid_id(lat,lng,grids):
     if not near_key2 or near_key2 - lng > 0.03:
         raise ValueError
     
-    return grids[near_key][near_key2]
+    gid = grids[near_key][near_key2]
+    return gid
 
 class Fire:
-    fire_types = []
-
     def __init__(self, event):
         self.id = event[0]
         self.time = event[1]
         self.lat = event[5]
         self.lng = event[6]
-        self.population = None
+        self.population = -1
         try:
             self.grid = get_grid_id(self.lat, self.lng, grids)
-            self.population = file_population_density[time_to_month(self.time)][self.grid][2]
+            m = time_to_month(self.time)
+            self.population = math.log10(file_population_density[time_to_month(self.time)][self.grid][2])
         except:
             self.grid = None
 
         # encode type, avoid Chinese
-        if event[2] in Fire.fire_types:
-            self.type = Fire.fire_types.index(event[2])
+        if event[2] in fire_types:
+            self.type = fire_types.index(event[2])
         else:
-            self.type = len(Fire.fire_types)
-            Fire.fire_types.append(event[2])
+            self.type = len(fire_types)
+            fire_types.append(event[2])
 
 
         self.main_station = "Not_found"
@@ -65,7 +68,6 @@ class Fire:
         else:
             self.support_station += event[3]
         
-        self.population = None
         self.temperature = None
         self.wet = None
     
@@ -75,11 +77,9 @@ class Fire:
         else:
             self.support_station += event[3]
     
-    def  __str__(self):
+    def js(self):
         # id, type, main_station, support_station, lat, lng, year, month, day_of_week, hour, waiting to add......
-        return str(self.id)+","+str(self.type)+","+self.main_station+","+self.support_station+","+str(self.lat)+","+str(self.lng)+","+\
-            str(self.time.year)+","+str(self.time.month)+","+str(self.time.dayofweek)+","+str(self.time.hour)+"\n"
-        
+        return {"id":self.id, "type":self.type, "day":self.time.dayofyear, "population":self.population, "year":self.time.year}
 
 for i in range(len(file_population_density[0])):
     if file_population_density[0][i][0] in grids.keys():
@@ -96,10 +96,13 @@ for event in file_fire.values:
         fires[event[0]] = Fire(event)
 
 
-for i in range(len(Fire.fire_types)):
-    print(i, Fire.fire_types[i])
+for i in range(len(fire_types)):
+    print(i, fire_types[i])
 
-with open("code\\data\\fire.csv", "w") as outfile:
-    outfile.write("id, type, main_station, support_station, lat, lng, year, month, day_of_week, hour\n")
-    for each_fire in fires.values():
-        outfile.write(str(each_fire))
+file = {"type": "FeatureCollection", "features": []}
+for i in fires.values():
+    if i.population != -1:
+        file["features"].append(i.js())
+
+with open("code\\data\\fire_mul.json", "w") as outfile:
+    json.dump(file, outfile)
