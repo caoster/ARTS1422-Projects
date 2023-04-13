8ded23640d3d6681ecd344e00005e5584f5e1a5e
caoster <caoster@icloud.com>
2023-04-13 12:50:34 +0800

    main.html

---
+++ code/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from markupsafe import Markup
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
