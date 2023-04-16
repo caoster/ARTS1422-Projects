ccef3b50936fe78298a1e6e90a072272d23b7a94
caoster <caoster@icloud.com>
2023-04-16 17:12:46 +0800

    process location and enterprise file

--- code/main.py
+++ code/main.py
@@ -26,28 +26,47 @@ for idx, event in fire.iterrows():
 
 fire_station = pd.read_excel("../data/fire_station.xlsx")
 fire_station['投用年月'] = fire_station['投用年月'].astype(str)
+fire_station['month'] = fire_station['投用年月'].map(lambda x: max(int(x.split('.')[1]), 1))
+fire_station['year'] = fire_station['投用年月'].map(lambda x: int(x.split('.')[0]))
 fire_station = dict(zip(fire_station['station_code'], fire_station.set_index('station_code').values.tolist()))
+
+population_industry: str
+with open("data/population_industry.csv", "r") as file:
+    population_industry = file.read()
 
 with open("token.secret") as token:
     TOKEN = token.read()
 
 
-@app.route("/_AMapService/<path:path>", methods=["GET", "POST"])
-def proxy(path):
-    SITE_NAME = "https://restapi.amap.com/"
+def redirect_proxy(site, path):
     arg = request.args.copy()
     arg.add("jscode", TOKEN)
     resp: Response.response
     if request.method == "GET":
-        resp = requests.get(f"{SITE_NAME}{path}", params=arg)
+        resp = requests.get(f"{site}{path}", params=arg)
     elif request.method == "POST":
-        resp = requests.post(f"{SITE_NAME}{path}", params=arg)
+        resp = requests.post(f"{site}{path}", params=arg)
     else:
         return Response(status=404)
     excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
     headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
     response = Response(resp.content, resp.status_code, headers)
     return response
+
+
+@app.route('/_AMapService/v4/map/<path:path>', methods=["GET", "POST"])
+def proxy_map(path):
+    return redirect_proxy("https://webapi.amap.com/v4/map/", path)
+
+
+@app.route("/_AMapService/<path:path>", methods=["GET", "POST"])
+def proxy(path):
+    return redirect_proxy("https://restapi.amap.com/", path)
+
+
+@app.route("/data/population_industry.csv")
+def population_industry_csv():
+    return population_industry
 
 
 @app.route("/data/fire_station.json")
